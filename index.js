67bc4e9b1545135e9593850f50f1f602816c3dc4
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-03-30 23:34:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,4 @@ function sayHiToGrandma(string){
   if(string === 'I love you, Grandma.'){
     return 'I love you, too.';
   }
-  else if(string===string.toLowerCase()){
-    return "I can't hear you!";
-  }
-  else(string===string.toUpperCase()){
-    return "YES INDEED!";
-  }
-
 }
